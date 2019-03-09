3bc8211e6585a0d22e183cd0257788d959d8cabe
Deepank Jain <deepankjain1501@gmail.com>
2019-03-09 17:30:48 +0530

    Python function to check whether a string is pangram or not

---
+++ Prog18.py
@@ -0,0 +1,10 @@
+#Python function to check whether a string is pangram or not
+def check_pangram(arg):
+  if len(set('abcdefghijklmnopqrstuvwxyz') - set(arg.lower())) == 0 :
+    return True
+  return False
+user_str = input("Enter a string to check for pangram : ")
+if(check_pangram(user_str)):
+  print("It is a pangram string")
+else:
+  print("Not a pangram string")
