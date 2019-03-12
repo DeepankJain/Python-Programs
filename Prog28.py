61ffd53c1826bfedfaeff6c8fa43cb1d15d5c548
Deepank Jain <deepankjain1501@gmail.com>
2019-03-12 17:37:16 +0530

    python program to print all prime numbers between 1 to 100

---
+++ Prog28.py
@@ -0,0 +1,10 @@
+#Program to print all prime numbers between 1 to 100
+for Number in range (1, 101):
+    count = 0
+    for i in range(2, (Number//2 + 1)):
+        if(Number % i == 0):
+            count = count + 1
+            break
+
+    if (count == 0 and Number != 1):
+        print(Number, end = '  ')
