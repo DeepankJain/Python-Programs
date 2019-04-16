39c0635724245870ec98665bd40d56f18cfb08c1
Deepank Jain <deepankjain1501@gmail.com>
2019-04-16 17:38:27 +0530

    Add files via upload

---
+++ inbuilt.py
@@ -0,0 +1,27 @@
+# my_list = list("deepankjain")
+# another_list = sorted(my_list)
+# print(another_list)
+# print(my_list)
+#
+# my_list.sort(reverse = True)
+# print(my_list)
+
+# name = ['x','y','z']
+# rating = [4,5,3]
+# new_list = list(zip(name,rating))
+# print(new_list[0][0])
+
+# def func(x):
+#     return x**3
+#
+# mylist = [12,43,654215484215412482656,1]
+# newlist = list(map(func,mylist))
+# print(newlist
+
+# newlist = list(map(int,input().split()))
+# print(newlist)
+
+myname = "Mohit Sharma"
+newname = set(myname)
+print(myname)
+print(newname)
